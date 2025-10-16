a9b05866e922f29a25542b4eb37ce1ce0611bedd
Pasindu <passbudd@gmail.com>
2025-10-16 10:46:08 +0530

    Add scanner

--- src/Bank.java
+++ src/Bank.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Bank {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("Hiii");
+        int number1 = scan.nextInt();
     }
 }
